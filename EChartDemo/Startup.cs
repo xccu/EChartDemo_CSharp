2f541274e0ad27c0b33e955cdd7a1cf086a77a96
Charlie <264672790@qq.com>
2020-05-27 19:49:12 +0800

    upload project

---
+++ EChartDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EChartDemo.Startup))]
+namespace EChartDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

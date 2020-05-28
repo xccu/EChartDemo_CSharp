9e52c1352bc8d8717c653e4b4d4f0914e22414df
Charlie <264672790@qq.com>
2020-05-28 19:43:45 +0800

    added PieLegend

--- EChartDemo/Controllers/HomeController.cs
+++ EChartDemo/Controllers/HomeController.cs
@@ -18,18 +18,10 @@ namespace EChartDemo.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult PieLegend()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
